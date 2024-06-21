d112184ad2cbe55bc5f954c5c94412939d9effb5
Harsh raj <harshrj9939@gmail.com>
2024-06-21 23:47:42 +0530

    optimised

--- powOf2.cpp
+++ powOf2.cpp
@@ -20,15 +20,12 @@ class Solution {
 int main() {
 
     int t;
-    cin >> t; // testcases
+    cin >> t;
 
     for (int i = 0; i < t; i++) {
         long long n; // input a number n
         cin >> n;
-        // if (n == 11) {
-        //     cout << "false" << endl;
-        //     return 0;
-        // }
+        
         Solution ob;
         if (ob.isPowerofTwo(n)) // Now, if log2 produces an integer not decimal then we
                                 // are sure raising 2 to this value
